4a055ab93ae67bf3eda6a6c759296eed0279b7e7
Ridwanur Rahman <ridwanur196@gmail.com>
2024-03-13 14:41:52 +0600

    pushed the result struct into monitors vector and converted the monitor into an json file

--- process_monitor_cli_app/src/main.rs
+++ process_monitor_cli_app/src/main.rs
@@ -1,23 +1,30 @@
-use serde::Deserialize;
+use serde::{Deserialize,Serialize};
 use std::env;
 use std::fs;
+use rand::Rng;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize,Serialize)]
 struct Monitor {
     monitor_id: Option<i32>,
     name: String,
     script: Option<String>,
-    result: Option<String>,
+    result: Option<MonitorResult>,
     #[serde(rename = "type")]
     monitor_type: Option<String>,
     code: String,
+    
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize,Serialize)]
 struct Monitors {
     monitors: Vec<Monitor>,
 }
-
+#[derive(Debug, Deserialize,Serialize)]
+struct MonitorResult{
+    value:i32,
+    processed_at:i32
+}
 fn parse_arguments() -> Result<Monitors, String> {
     let args: Vec<String> = env::args().collect();
 
@@ -49,26 +56,31 @@ fn parse_arguments() -> Result<Monitors, String> {
 }
 
 fn main() {
-    let monitors: Monitors;
+    let mut monitors: Monitors;
 
     match parse_arguments() {
         Ok(parsed_monitors) => {
             monitors = parsed_monitors;
-             println!("Number of monitors: {}", monitors.monitors.len());
-             for monitor in &monitors.monitors {
-                println!("Monitor ID: {:?}", monitor.monitor_id);
-                println!("Name: {}", monitor.name);
-                println!("Script: {:?}", monitor.script);
-                println!("Result: {:?}", monitor.result);
-                println!("Monitor Type: {:?}", monitor.monitor_type);
-                println!("Code: {}", monitor.code);
+            println!("Number of monitors: {}", monitors.monitors.len());
+
+            for monitor in &mut monitors.monitors {
+                let result = generate_result(); 
+                monitor.result = Some(result); 
             }
+            
+            let json_string = serde_json::to_string_pretty(&monitors).unwrap();
+            std::fs::write("output.json", json_string).expect("Unable to write file");
         }
         Err(err) => {
             eprintln!("{}", err);
             return;
         }
     }
- 
-    
+}
+fn generate_result() -> MonitorResult {
+    let value = rand::thread_rng().gen_range(1..100);
+    let current_time = SystemTime::now();
+    let since_epoch = current_time.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let processed_at = since_epoch.as_secs() as i32; 
+    MonitorResult { value, processed_at }
 }
