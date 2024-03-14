f92d889c00165ca54ee4194f618c6d8c8108a87f
Ridwanur Rahman <ridwanur196@gmail.com>
2024-03-14 13:00:36 +0600

    step 6 implemented

--- process_monitor/src/main.rs
+++ process_monitor/src/main.rs
@@ -1,10 +1,13 @@
-use serde::{Deserialize,Serialize};
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs;
 use rand::Rng;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::thread;
+use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
 
-#[derive(Debug, Deserialize,Serialize)]
+#[derive(Debug, Deserialize,Clone, Serialize)]
 struct Monitor {
     monitor_id: Option<i32>,
     name: String,
@@ -13,25 +16,25 @@ struct Monitor {
     #[serde(rename = "type")]
     monitor_type: Option<String>,
     code: String,
-    
 }
 
-#[derive(Debug, Deserialize,Serialize)]
+#[derive(Debug, Deserialize,Clone, Serialize)]
 struct Monitors {
     monitors: Vec<Monitor>,
 }
-#[derive(Debug, Deserialize,Serialize)]
-struct MonitorResult{
-    value:i32,
-    processed_at:i32
+
+#[derive(Debug, Deserialize,Clone, Serialize)]
+struct MonitorResult {
+    value: i32,
+    processed_at: i32,
 }
+
 fn parse_arguments() -> Result<Monitors, String> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
         return Err("Insufficient arguments provided. Usage: -monitorFile <file_path>".to_string());
     }
-
 
     if args[1] != "-monitorFile" {
         return Err("Invalid argument. Second argument must be '-monitorFile'.".to_string());
@@ -45,7 +48,6 @@ fn parse_arguments() -> Result<Monitors, String> {
     let file_content = fs::read_to_string(file_path)
         .map_err(|err| format!("Error reading file: {}", err))?;
 
-  
     let monitors: Monitors = serde_json::from_str(&file_content)
         .map_err(|err| format!("Error parsing JSON: {}", err))?;
 
@@ -59,25 +61,96 @@ fn main() {
         Ok(parsed_monitors) => {
             monitors = parsed_monitors;
             println!("Number of monitors: {}", monitors.monitors.len());
-
-            for monitor in &mut monitors.monitors {
-                let result = generate_result(); 
-                monitor.result = Some(result); 
-            }
+            let shared_monitors = Arc::new(Mutex::new(monitors.clone())); 
+            process_monitors(shared_monitors);
             
-            let json_string = serde_json::to_string_pretty(&monitors).unwrap();
-            std::fs::write("output.json", json_string).expect("Unable to write file");
+           
         }
         Err(err) => {
             eprintln!("{}", err);
             return;
         }
+        
     }
 }
+
+fn update_monitors(shared_monitors: Arc<Mutex<Monitors>>) {
+    let start_time = Instant::now();
+
+    loop {
+       
+        if start_time.elapsed() >= Duration::from_secs(300) {
+            break;
+        }
+
+        
+        thread::sleep(Duration::from_secs(30));
+        let mut monitors = shared_monitors.lock().unwrap();
+        for monitor in &mut monitors.monitors {
+            let result = generate_result();
+            monitor.result = Some(result);
+        }
+        println!("{:#?}", monitors);
+    }
+}
+
+
+fn store_monitors(shared_monitors: Arc<Mutex<Monitors>>) {
+    let start_time = Instant::now();
+
+    loop {
+        let elapsed_time = start_time.elapsed();
+        if elapsed_time >= Duration::from_secs(300) {
+            break;
+        }
+
+        
+        thread::sleep(Duration::from_secs(60));
+
+        let monitors = shared_monitors.lock().unwrap();
+        let json_string = serde_json::to_string_pretty(&*monitors).unwrap();
+        let current_timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        let file_name = format!("{}_monitors.json", current_timestamp);
+        fs::write(&file_name, json_string).expect("Unable to write file");
+    }
+}
+
+
+fn process_monitors(shared_monitors: Arc<Mutex<Monitors>>) {
+    let start_time = Instant::now();
+
+    loop {
+       
+        if start_time.elapsed() >= Duration::from_secs(300) {
+            break; 
+        }
+
+        
+        let update_thread = thread::spawn({
+            let shared_monitors = Arc::clone(&shared_monitors);
+            move || {
+                update_monitors(shared_monitors);
+            }
+        });
+
+        let store_thread = thread::spawn({
+            let shared_monitors = Arc::clone(&shared_monitors);
+            move || {
+                store_monitors(shared_monitors);
+            }
+        });
+
+
+        update_thread.join().unwrap();
+        store_thread.join().unwrap();
+    }
+}
+
+
 fn generate_result() -> MonitorResult {
     let value = rand::thread_rng().gen_range(1..100);
     let current_time = SystemTime::now();
     let since_epoch = current_time.duration_since(UNIX_EPOCH).expect("Time went backwards");
-    let processed_at = since_epoch.as_secs() as i32; 
+    let processed_at = since_epoch.as_secs() as i32;
     MonitorResult { value, processed_at }
 }
